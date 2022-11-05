f8833b921baa6103f8a6381730a8cee1ba0afd50
Daniil <daniil.okrug@gmail.com>
2022-11-05 23:15:01 +0300

    Axios fix

--- src/axios/axios.config.ts
+++ src/axios/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "", // !! Server URL
+  baseURL: "https://plony.ru", // !! Server URL
 });
 
 /*
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,6 +3,6 @@ import $api from "../axios/axios.config";
 
 export default class UserService {
   static async requestData(): Promise<AxiosResponse<any>> {
-    return $api.get<any>("https://plony.ru/geo/ao.json");
+    return $api.get<any>("/geo/ao.json");
   }
 }
